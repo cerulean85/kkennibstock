5fcfecf771224e38cf5ffffd2cd54d41771e8412
cerulean85 <zhwan85@gmail.com>
2025-07-02 20:44:24 +0900

    Nav 버튼 안 나오는 오류 수정

--- components/ClientRootLayout.tsx
+++ components/ClientRootLayout.tsx
@@ -526,7 +526,7 @@ export default function ClientRootLayout({ children }: { children: React.ReactNo
                           {currentMenu === Menu.TextMining && pageKey === Page.TextMiningDashboard && (
                             <button
                               type="button"
-                              className="w-full flex items-center justify-center gap-2 bg-gradient-to-r from-blue-700 to-blue-500 text-white px-5 py-2 rounded-lg font-bold text-base shadow-md hover:from-blue-900 hover:to-sky-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-200"
+                              className="w-full flex items-center justify-center gap-2 bg-gradient-to-r from-blue-700 to-blue-500 text-white px-5 py-2 rounded-lg font-bold text-base shadow-md hover:from-blue-900 hover:to-sky-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-200 my-2"
                               onClick={() => setAddNewWorkOpen(true)}
                             >
                               <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
